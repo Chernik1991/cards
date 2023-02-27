a42610685b7a44be470b79858f199e79f703caf8
MAKS <5059585@mail.ru>
2023-02-27 13:30:23 +0300

    init commit

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-{/*//hr, prov*/}
-      <>
-        {/*<Header/>*/}
-          hhhhhh
-          {/*<Main/>*/}
-          {/*<Main/>*/}
-    </>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*//hr, prov*/}
+            {/*<Header/>*/}
+            hhhhhh
+            {/*<Main/>*/}
+            {/*<Main/>*/}
+        </div>
+    );
 }
 
 export default App;
