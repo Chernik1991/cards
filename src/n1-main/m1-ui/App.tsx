ba3abfd09541e806d410f2c0a6283efbcb27d89d
MAKS <5059585@mail.ru>
2023-02-27 16:07:33 +0300

    init commit

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Pages} from './routes/pages';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            {/*<BrowserRouter>*/}
+                <HashRouter>
                 {/*<Layout>*/}
                     <Pages/>
                 {/*</Layout>*/}
-            </BrowserRouter>
+            </HashRouter>
+            {/*</BrowserRouter>*/}
         </div>
     );
 }
