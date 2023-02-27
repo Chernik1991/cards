12a425f00d4c5159ee2d0a8f9537c20d15921674
MAKS <5059585@mail.ru>
2023-02-27 11:35:02 +0300

    init commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+//hr, prov
+      <>
+        {/*<Header/>*/}
+          {/*<Main/>*/}
+          {/*<Main/>*/}
+    </>
+    </div>
+  );
+}
+
+export default App;
