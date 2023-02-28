93e557df817ead4c5cf8f15af9757c2108d52e31
MAKS <5059585@mail.ru>
2023-02-28 19:53:56 +0300

    build(init): init
    init

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -7,6 +7,8 @@ export const appReducer = (
   switch (action.type) {
     case 'APP/SET-STATUS':
       return { ...state, status: action.status }
+    case 'APP/SET-ERROR':
+      return { ...state, error: action.error }
     default:
       return { ...state }
   }
@@ -14,9 +16,11 @@ export const appReducer = (
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 export type InitialStateType = {}
+export const setAppErrorAC = (error: string | null) => ({ type: 'APP/SET-ERROR', error } as const)
 export const setAppStatusAC = (status: RequestStatusType) =>
   ({ type: 'APP/SET-STATUS', status } as const)
 
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 
-type ActionsType = SetAppStatusActionType
+type ActionsType = SetAppStatusActionType | SetAppErrorActionType
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
+import thunkMiddleware, { ThunkDispatch } from 'redux-thunk'
 
 import { appReducer } from './app-reducer'
 
@@ -13,7 +14,10 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
-
+// создаем тип диспатча который принимает как AC так и TC
+export type AppThunk = ThunkDispatch<AppRootStateType, any, AnyAction>
+export const useAppDispatch = () => useDispatch<AppThunk>()
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore.
 window.store = store
