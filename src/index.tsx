1fc976a738a46f8b904b45d65bd8cefab4ec3cb8
MAKS <5059585@mail.ru>
2023-03-03 13:39:18 +0300

    build(init): init
    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
+// eslint-disable-next-line import/order
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { Provider } from 'react-redux'
+
 import App from './app/App'
-import reportWebVitals from './reportWebVitals'
+import { store } from './app/store'
 
 // eslint-disable-next-line import/no-named-as-default-member
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
