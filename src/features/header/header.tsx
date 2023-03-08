fce66571ac87e96b68dbf8e4cf457d4b47607c0f
MAKS <5059585@mail.ru>
2023-03-08 17:21:43 +0300

    feat(merge): merge

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,57 +1,34 @@
 import React from 'react'
 
-import { AppBar, Button, IconButton, LinearProgress, Menu, Toolbar } from '@mui/material'
-import Typography from '@mui/material/Typography'
-import { NavLink } from 'react-router-dom'
+import { Button } from '@mui/material'
 
-import { RequestStatusType } from 'app/app-reducer'
-import { useAppDispatch, useAppSelector } from 'app/store'
-import { PATH } from 'common/components/Routing/pages'
-import { logoutTC } from 'features/auth/login/auth-reducer'
+import itINC from '../../assets/img/icons/itINC.svg'
 
-const Header = () => {
-  const status = useAppSelector<RequestStatusType>(state => state.app.status)
-  const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
-  const dispatch = useAppDispatch()
-  const logOutHandler = () => {
-    dispatch(logoutTC())
-  }
+import s from './HeaderStyles.module.css'
 
+export const Header = () => {
   return (
-    <>
-      {/*<ErrorSnackbar />*/}
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu open={false} />
-          </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Typography variant="h6">
-            {isLoggedIn && (
-              <Button color="inherit" onClick={logOutHandler}>
-                Log out
-              </Button>
-            )}
-          </Typography>
-          {/*{isLoggedIn && (*/}
-          {/*  <Button color="inherit" onClick={logOutHandler}>*/}
-          {/*    Log out*/}
-          {/*  </Button>*/}
-          {/*)}*/}
-        </Toolbar>
-        {status === 'loading' && <LinearProgress />}
-      </AppBar>
-      <div>header Layout</div>
-      <NavLink to={PATH.LOGIN}>Login</NavLink>
-      <NavLink to={PATH.REGISTER}>Register</NavLink>
-      <NavLink to={PATH.PROFILE}>Profile</NavLink>
-      <NavLink to={PATH.FORGOT_YOUR_PASSWORD}>Forgot Password</NavLink>
-      <NavLink to={PATH.CHECK_EMAIL}>Check Email</NavLink>
-      <NavLink to={PATH.CREATE_NEW_PASSWORD}>Create New Password</NavLink>
-      <NavLink to={PATH.TEST}>Test</NavLink>
-      <NavLink to={'*'}>Error404</NavLink>
-    </>
+    <div>
+      <div className={s.headerBlock}>
+        <img alt={'IMG'} src={itINC} />
+
+        <Button
+          variant="contained"
+          sx={{
+            borderRadius: '20px',
+            minWidth: '115px',
+            fontSize: '16px',
+            lineHeight: '20px',
+            fontFamily: 'Montserrat, sans-serif',
+            fontStyle: 'Medium',
+            textTransform: 'none',
+          }}
+        >
+          <a className={s.headerA} href={'/login'}>
+            Sign In
+          </a>
+        </Button>
+      </div>
+    </div>
   )
 }
-
-export default Header
